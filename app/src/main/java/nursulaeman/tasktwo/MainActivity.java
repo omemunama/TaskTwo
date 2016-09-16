d3d5725015afb8b242e5ba1ef845a7ed3a07462b
omemunama <xnursx@gmail.com>
2016-09-17 00:01:09 +0700

    using glide for MainActivity

--- app/src/main/java/nursulaeman/tasktwo/MainActivity.java
+++ app/src/main/java/nursulaeman/tasktwo/MainActivity.java
@@ -2,6 +2,10 @@ package nursulaeman.tasktwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends BaseActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.my_image_view);
+
+        String url1 = "https://blog.wgs.co.id/wp-content/uploads/2015/12/Logo-Green-Grey.png";
+
+        Glide.with(this)
+                .load(url1)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);
+
     }
 }
