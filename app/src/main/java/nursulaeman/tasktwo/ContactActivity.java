fce3b5abb28851e1a3adc8ad7a8a2e9eb276e752
omemunama <xnursx@gmail.com>
2016-09-17 09:57:43 +0700

    tab layout in ContactActivity

--- app/src/main/java/nursulaeman/tasktwo/ContactActivity.java
+++ app/src/main/java/nursulaeman/tasktwo/ContactActivity.java
@@ -1,5 +1,6 @@
 package nursulaeman.tasktwo;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class ContactActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Address"));
+        tabLayout.addTab(tabLayout.newTab().setText("Phone"));
+        tabLayout.addTab(tabLayout.newTab().setText("Other"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
     }
 }
